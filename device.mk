4927461c5286763bb534e594ddb5b5842efd8526
Fraschze97 <Anprasetya97@gmail.com>
2022-05-07 20:22:50 +0100

    apollow: Add priv-app permissions for IMS
    Change-Id: I856f1640f72a6b074e5390c2f4577efe3beb2d6b

--- device.mk
+++ device.mk
@@ -124,6 +124,10 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# IMS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor \
