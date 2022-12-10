57146c37f46407e5a6fc674e7342aabe47607dbc
bengris32 <bengris32@protonmail.ch>
2022-12-10 10:12:13 +0000

    apollow: Initial skeleton
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Id75dad871d699d4247d8753be6ec6217644a8eef

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_apollow.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_apollow-user \
+    lineage_apollow-userdebug \
+    lineage_apollow-eng
---
+++ README.md
@@ -0,0 +1,9 @@
+# Android Device tree for realme 8s 5G
+
+```
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
