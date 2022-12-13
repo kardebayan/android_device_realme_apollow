b5bdeeaba256ea15500a2137b5787c5e9db8db8a
bengris32 <bengris32@protonmail.ch>
2022-12-13 21:04:49 +0000

    apollow: Build required USB interfaces
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I431f76c1a62d24c988c2dfaa5867441adf5c7458

--- device.mk
+++ device.mk
@@ -238,6 +238,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3.vendor \
+    android.hardware.usb.gadget@1.1.vendor
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.5.vendor \
