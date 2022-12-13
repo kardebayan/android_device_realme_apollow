bc21c847c7d91a89432b26fcc06b4fec54eaa9a9
bengris32 <bengris32@protonmail.ch>
2022-12-13 19:58:46 +0000

    apollow: Build default Composer 2.3 service
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Ieedbbd72fd16ba97a574a935b43560ec3e942261

--- device.mk
+++ device.mk
@@ -50,6 +50,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0.vendor \
     android.hardware.graphics.allocator@3.0.vendor \
     android.hardware.graphics.allocator@4.0.vendor \
+    android.hardware.graphics.composer@2.3-service \
     libdrm.vendor
 
 # fastbootd
