af8e2ab0c775769b7ead108091ab06cbb40c5587
bengris32 <bengris32@protonmail.ch>
2022-12-13 21:00:24 +0000

    apollow: Build required Neural Network interfaces/libs
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Idfee84659ebb5bf037cfae5512128aae75824016

--- device.mk
+++ device.mk
@@ -155,6 +155,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Neural Networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3.vendor \
+    libtextclassifier_hash.vendor
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
