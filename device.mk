8367fd89409618274becd1e1751a4f5d5380f035
Debayan Kar <kardebayan3@gmail.com>
2023-04-26 16:20:16 +0530

    apollow: Import public.libraries.txt from stock

--- device.mk
+++ device.mk
@@ -154,6 +154,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor
 
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
