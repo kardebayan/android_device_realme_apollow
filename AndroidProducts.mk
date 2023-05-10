297e71d05adcd5fd9d92e9e20d9daec044f648b7
Debayan Kar <69067988+kardebayan@users.noreply.github.com>
2023-05-10 13:27:54 +0000

    apollow: AOSP-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The PixelOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollow.mk
+    $(LOCAL_DIR)/aosp_apollow.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollow-user \
-    lineage_apollow-userdebug \
-    lineage_apollow-eng
+    aosp_apollow-user \
+    aosp_apollow-userdebug \
+    aosp_apollow-eng
--- aosp_apollow.mk
+++ aosp_apollow.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The aospOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,16 +12,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/realme/apollow/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_apollow
+PRODUCT_NAME := aosp_apollow
 PRODUCT_DEVICE := apollow
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme 8s 5G
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
-
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
